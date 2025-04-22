fa0d9353fd0c06e7951414e530a3606b84dd58cc
Throuz <zed2786328@gmail.com>
2025-04-22 22:14:55 +0800

    init

---
+++ programs/reflink-anchor/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("4PpiC9e179ufENLcgmK5NQJKHZ48NepLguqCJPsnBT9A");
+
+#[program]
+pub mod reflink_anchor {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
